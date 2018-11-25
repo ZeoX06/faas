f9a79c1aa66e2c82a2ecd13d9bdce35203711883
nils <nil@zeropi.home>
2018-11-25 22:36:00 +0000

    changed deploy_stack script for armv6

--- deploy_stack.armv6.sh
+++ deploy_stack.armv6.sh
@@ -5,5 +5,18 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 1
 fi
 
-echo "Deploying stack"
+####################
+# ToDo: remove later, building of armv6 containers
+echo "Build arm containers"
+faas_home=$(pwd)
+
+# building openfaas/gateway:latest-armv6-dev
+cd gateway && ./build.sh
+cd $faas_home
+
+# building arm32v6/prometheus:2.0
+cd contrib/prometheus-armv6/2.0 && docker-compose build && cd $faas_home
+####################
+
+#echo "Deploying stack"
 docker stack deploy func --compose-file docker-compose.armv6.yml
